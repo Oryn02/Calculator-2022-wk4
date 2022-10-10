9656fa1d96cc2dc539cbb105aadab261a4a48b20
Rakib <G00408644@atu.ie>
2022-10-10 10:14:59 +0100

    added new feature subtract

--- src/main/java/ie/atu/week4Calc22/Calc22.java
+++ src/main/java/ie/atu/week4Calc22/Calc22.java
@@ -1,9 +1,22 @@
 package ie.atu.week4Calc22;
+import java.util.Scanner;
 
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Calc22
 {
+    public static void main(String[] args) {
+        System.out.println("Please enter your number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("PLease enter your second number: ");
+        int SecondNumber = inputs.nextInt();
+        Scanner input = new Scanner(System.in);
+
+        int total = firstNumber - SecondNumber;
+        System.out.println("The total is " + total);
+    }
 
 }
