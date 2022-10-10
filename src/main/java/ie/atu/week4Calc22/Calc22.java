2c8154d963b3197dab0d26a93644ddb8bbb00366
Rakib <G00408644@atu.ie>
2022-10-10 10:41:36 +0100

    updated feature v2

--- src/main/java/ie/atu/week4Calc22/Calc22.java
+++ src/main/java/ie/atu/week4Calc22/Calc22.java
@@ -1,22 +1,24 @@
 package ie.atu.week4Calc22;
 import java.util.Scanner;
 
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
 public class Calc22
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        subtract();
+    }
+
+    //This method takes 2 values away from each other
+    public static void subtract()
+    {
         System.out.println("Please enter your number: ");
         Scanner inputs = new Scanner(System.in);
         int firstNumber = inputs.nextInt();
 
         System.out.println("PLease enter your second number: ");
         int SecondNumber = inputs.nextInt();
-        Scanner input = new Scanner(System.in);
 
         int total = firstNumber - SecondNumber;
         System.out.println("The total is " + total);
     }
-
 }
