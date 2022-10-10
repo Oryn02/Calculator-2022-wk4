1f1330fea101233a1f09b61f3141ade1c1ce65f1
Artemiy <G00404444@atu.ie>
2022-10-10 10:16:44 +0100

    Added feature multiply

--- src/main/java/ie/atu/week4Calc22/Calc22.java
+++ src/main/java/ie/atu/week4Calc22/Calc22.java
@@ -1,9 +1,23 @@
 package ie.atu.week4Calc22;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Calc22
 {
+    public static void main(String[] args) {
+        add();
 
+    }
+    public static void add()
+    {
+        System.out.println("Please enter first number");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter second number");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
+        System.out.println("The total is: " + total);
+    }
 }
