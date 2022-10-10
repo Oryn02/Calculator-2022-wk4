a48f2909700362b681c2ac82d669c30b4ac6f9fe
Oryn Quinlan <g00406032@atu.ie>
2022-10-10 10:49:14 +0100

    bug fixed 1.0.2 patch

--- src/main/java/ie/atu/week4Calc22/Calc22.java
+++ src/main/java/ie/atu/week4Calc22/Calc22.java
@@ -7,13 +7,14 @@ public class Calc22
     public static void main(String[] args) 
     {
       add();
-      multiply();
       subtract();
+      multiply();
     }
     
     //This method takes in a value and multiplies
     public static void multiply()
     {
+        System.out.println("You are now multiplying");
         System.out.println("Please enter first number");
         Scanner inputs = new Scanner(System.in);
         int firstNumber = inputs.nextInt();
@@ -28,6 +29,7 @@ public class Calc22
     //This method takes in a value and adds
     public static void add()
     {
+        System.out.println("You are now adding");
         System.out.println("please enter first number");
         Scanner input = new Scanner(System.in);
         int firstNumber = input.nextInt();
@@ -40,7 +42,9 @@ public class Calc22
     }
     
     //This method takes 2 values away from each other
-    public static void subtract() {
+    public static void subtract()
+    {
+        System.out.println("You are now subtracting");
         System.out.println("Please enter your number: ");
         Scanner inputs = new Scanner(System.in);
         int firstNumber = inputs.nextInt();
@@ -49,5 +53,6 @@ public class Calc22
         int SecondNumber = inputs.nextInt();
 
         int total = firstNumber - SecondNumber;
+        System.out.println("The total is " + total);
     }
 }
